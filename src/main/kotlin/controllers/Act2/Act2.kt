032a143a91d280d62f9e2ee840405f8b52ac1fc2
anmolpreet.singh.7e8 <anmolpreet.singh.7e8@itb.cat>
2024-12-10 13:59:06 +0100

    Act 5 sin tests ni dokka

--- src/main/kotlin/controllers/Act2/Act2.kt
+++ src/main/kotlin/controllers/Act2/Act2.kt
@@ -1,11 +1,12 @@
 package controllers.Act2
 
 import java.util.*
+import kotlin.math.sign
 
 fun main(){
     val scanner = abrirScanner()
     val nums = pedimosNumeroEntero(scanner)
-    val cambio = canviEnterARoman(nums)
+    val cambio = cencontrarUnidades(nums)
     cerrarScanner(scanner)
 }
 
@@ -22,12 +23,25 @@ fun pedimosNumeroEntero(scanner:Scanner): Int{
     return num
 }
 
-fun canviEnterARoman(nums: Int):String{
+fun cencontrarUnidades(nums: Int):String{
     var numRoman = ""
-//    for(i in nums){
-//        if(i == 1){
-            numRoman += 'I'
-//        }
-    //}
+    val unidades = arrayOf<String>("" ,"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+    val decenas = arrayOf<String>("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",)
+    val centenas = arrayOf<String>("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",)
+    val miles = arrayOf<String>("", "M", "MM", "MMM")
+
+    val millar = nums/1000
+    print(miles[millar])
+
+    val calculoCentenas = (nums/100) % 10
+    print(centenas[calculoCentenas])
+
+    val calculoDecenas = (nums/10) % 10
+    print(decenas[calculoDecenas])
+
+    val calculoUniades = nums%10
+    print(unidades[calculoUniades])
+
+    println(numRoman)
     return numRoman
 }
