7b1369cc33a9a30cb1a947523b4a71983cbdcf17
carlos.vargas <carlos.vargas.7e8@itb.cat>
2024-12-10 20:21:57 +0100

    ejercicio1 y comentarios

--- src/main/kotlin/controllers/Act1/Act1.kt
+++ src/main/kotlin/controllers/Act1/Act1.kt
@@ -1,32 +1,38 @@
 package controllers.Act1
-
 import controllers.MensajesError.letra_no_Entero
 import java.util.*
+import kotlin.math.roundToInt
 
 fun main(){
     val scan = Scanner(System.`in`)
-    print("Ingrese el precio al cual quiere aplicar el iva: ")
-    var precio = scan.nextDouble()
-    println()
-
+    var precio = recogerDouble("Ingrese el precio al cual quiere aplicar el iva: ", scan)
+    scan.nextLine()
     print("Ingresa la fecha de compra con el formato DD/MM/YY: ")
     val fecha = scan.nextLine().split("/").map { it.toInt() }
     println()
+    //datos de la fecha
     val dia = fecha[0]
     val mes = fecha[1]
     val año = fecha[2]
-    menuIvas()
+    menuIvas() //mostrar las opciones disponible al usuario
     val elecion = scan.nextInt()
-
+    val totalIva= calcularIVA(elecion, precio, año)
+    println(totalIva)
 }
-fun recogerEntero(msg: String, scan: Scanner) :Int {
+/**
+ * @author Carlos Vargas
+ * @param msg Mensaje que ofrece en que contexto se usara el Double ingresado  por el usuario
+ * @param scan Permite escanear el teclado del usuario
+ * @return Rertona el numero DOuble ingresado por el usuario, si lo que ingresa no es valido se retornara un Double.MAX_VALUE
+ */
+fun recogerDouble(msg: String, scan: Scanner) :Double {
     print(msg)
-    var numero :Int
+    var numero :Double
     try {
-        numero =scan.nextInt()
+        numero =scan.nextDouble()
     }
     catch (errorLetra : InputMismatchException){
-        numero= Int.MAX_VALUE
+        numero= Double.MAX_VALUE
         letra_no_Entero()
     }
     return numero
@@ -38,20 +44,42 @@ fun menuIvas(){
     println("4) Excento")
     print("Seleccione cual es el iva que quiere aplicar: ")
 }
-fun calcularIVA(eleccion:Int, precio:Double){
+/**
+ * @author Carlos Vargas
+ * @param eleccion Numero que indica la opción elegida por el usuario, dependiendo de su valor se udar una funcion diferente
+ * @param precio Precio original que indico el usuario
+ * @param fecha año del iva que se quiere aplicar
+ * @return Rertona el valor del iva redondeado a dos decimales, el valor dependera del la eleccion del usuario.
+ */
+fun calcularIVA(eleccion:Int, precio:Double, fecha:Int) :Double{
+    var iva =0.0
     if (eleccion ==1){
-
+        iva = ivaGeneral(precio, fecha)
     }
     else if (eleccion==2){
-
+        iva = ivaReducido(precio, fecha)
     }
     else if (eleccion==3){
-
+        iva = ivaSuperReducido(precio, fecha)
     }
-    else if (eleccion==4)
+    else if (eleccion==4){
+        iva = precio*0.0
+    }
+    else{
+        println("Opcion no valida")
+    }
+    iva = (iva * 100.0).roundToInt() / 100.0
+    return iva
 }
+/**
+ * @author Carlos Vargas
+ * @param precio Precio original que indico el usuario
+ * @param fecha año del iva que se quiere aplicar
+ * @return Rertona el valor del iva dependiedno de su año de aplicacion.
+ */
 fun ivaGeneral(precio: Double, fecha:Int):Double{
     var iva:Double
+    iva=0.0
     if (fecha<1992){
         iva=precio*0.12
     }
@@ -60,6 +88,55 @@ fun ivaGeneral(precio: Double, fecha:Int):Double{
     }
     else if (fecha in 1995 until 2010){
         iva=precio*0.16
+    }
+    else if (fecha in 2010 until 2012){
+        iva=precio*0.18
+    }
+    else{
+        iva=precio*0.21
+    }
+    return iva
+}
+/**
+ * @author Carlos Vargas
+ * @param precio Precio original que indico el usuario
+ * @param fecha año del iva que se quiere aplicar
+ * @return Rertona el valor del iva dependiedno de su año de aplicacion.
+ */
+fun ivaReducido(precio: Double, fecha:Int):Double{
+    var iva:Double
+    iva=0.0
+    if (fecha < 1995){
+        iva=precio*0.6
+    }
+    else if (fecha in 1995 until 2010){
+        iva=precio * 0.7
+    }
+    else if (fecha in 2010 until 2012){
+        iva=precio*0.16
+    }
+    else{
+        iva=precio*0.21
+    }
+    return iva
+}
+/**
+ * @author Carlos Vargas
+ * @param precio Precio original que indico el usuario
+ * @param fecha año del iva que se quiere aplicar
+ * @return Rertona el valor del iva dependiedno de su año de aplicacion.
+ */
+fun ivaSuperReducido(precio: Double, fecha:Int):Double{
+    var iva:Double
+    iva=0.0
+    if (fecha < 1993){
+        iva=precio*0.0
+    }
+    else if (fecha in 1993 until 1995){
+        iva=precio * 0.3
+    }
+    else{
+        iva=precio*0.4
     }
     return iva
 }
