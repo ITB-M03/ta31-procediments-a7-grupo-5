427b2da537c66d11045072a67f7ea97e334e516b
anmolpreet.singh.7e8 <anmolpreet.singh.7e8@itb.cat>
2024-12-10 11:41:44 +0100

    Organizacion2

--- src/main/kotlin/controllers/Act2/Act2.kt
+++ src/main/kotlin/controllers/Act2/Act2.kt
@@ -1,5 +1,27 @@
-package org.example.controllers
+package controllers.Act2
 
-fun main() {
-    //TODO
+import java.util.*
+
+fun main(){
+    val scanner = abrirScanner()
+    val num1 = pedimosNumeroEntero(scanner)
+
+    cerrarScanner(scanner)
+}
+
+fun abrirScanner( ): Scanner{
+    return Scanner(System.`in`)
+}
+
+fun cerrarScanner(scanner: Scanner){
+    scanner.close()
+}
+
+fun pedimosNumeroEntero(scanner:Scanner): Int{
+    val num = scanner.nextInt()
+    return num
+}
+
+fun canviEnterARoman(){
+
 }
--- src/main/kotlin/controllers/Act3/Act3.kt
+++ src/main/kotlin/controllers/Act3/Act3.kt
@@ -1,2 +1,23 @@
 package controllers.Act3
 
+import java.util.*
+
+fun main(){
+    val scanner = abrirScanner()
+    val num1 = pedimosNumeroEntero(scanner)
+
+    cerrarScanner(scanner)
+}
+
+fun abrirScanner( ): Scanner{
+    return Scanner(System.`in`)
+}
+
+fun cerrarScanner(scanner: Scanner){
+    scanner.close()
+}
+
+fun pedimosNumeroEntero(scanner:Scanner): Int{
+    val num = scanner.nextInt()
+    return num
+}
