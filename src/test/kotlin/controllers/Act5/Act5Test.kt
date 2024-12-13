fb53050b548b57065fe02148c96c111b64656e9e
anmolpreet.singh.7e8 <anmolpreet.singh.7e8@itb.cat>
2024-12-13 09:42:16 +0100

    Act 5 final con dokka y test

---
+++ src/test/kotlin/controllers/Act5/Act5Test.kt
@@ -0,0 +1,12 @@
+package controllers.Act5
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class Act5Test {
+
+@Test
+ fun menuFinal() { //No se pueden hacer los test porque casi cada funcion tiene un scanner o no hace ningun return.
+  val esperat = " "
+ }
+}
